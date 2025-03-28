ea2009ad05ffea80f7e3aead19e2117496f7cd73
12PUffS <evgenypech98@gmail.com>
2025-03-28 19:21:31 +0300

    brain-even.js , cli.js update

--- src/cli.js
+++ src/cli.js
@@ -1,14 +1,11 @@
 import readlineSync from 'readline-sync';
 
-const sayHi = () => {
+export const sayHi = () => {
   console.log("Welcome to the Brain Games!");
 };
 
-const getName = () => {
+export const getName = () => {
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   return name;
 };
-
-// Экспортируем оба метода
-export { sayHi, getName };
--- src/games/even.js
+++ src/games/even.js
@@ -1,40 +1,34 @@
 import readlineSync from 'readline-sync';
-import getName from '../cli.js';
+import { sayHi } from '../cli.js';
+import { getName } from '../cli.js';
 import { getRandomNumber } from '../index.js';
 
-
 const playEvenGame = () => {
-  
   const ROUNDS_COUNT = 3;
-  
-  // Инициализация
-const userName = getName();
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  sayHi()
 
-// Основной игровой цикл
-let correctAnswers = 0;
-while (correctAnswers < ROUNDS_COUNT) {
-  // Генерация вопроса
-  const number = getRandomNumber(0, 100)
-  console.log(`Question: ${number}`);
-  
-  // Проверка ответа
-  const userAnswer = readlineSync.question('Your answer: ').toLowerCase();
-  const isEven = number % 2 === 0;
-  const correctAnswer = isEven ? 'yes' : 'no';
+  const userName = getName();
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-  if (userAnswer === correctAnswer) {
-    console.log('Correct!');
-    correctAnswers += 1;
-  } else {
-    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-    console.log(`Let's try again, ${userName}!`);
-    process.exit(1); // Завершаем игру при ошибке
+  for (let correctAnswers = 0; correctAnswers < ROUNDS_COUNT;) {
+    const number = getRandomNumber(0, 100);
+    console.log(`Question: ${number}`);
+    
+    const userAnswer = readlineSync.question('Your answer: ').toLowerCase();
+    const isEven = number % 2 === 0;
+    const correctAnswer = isEven ? 'yes' : 'no';
+
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!');
+      correctAnswers++;
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
+    }
   }
-}
 
-// Поздравление при победе
-console.log(`Congratulations, ${userName}!`);    
-}
+  console.log(`Congratulations, ${userName}!`);
+};
 
-export default playEvenGame
+export default playEvenGame;
