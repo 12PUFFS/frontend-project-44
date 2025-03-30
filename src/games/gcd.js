98763c0103335428d0da4e7cae1681d123584b79
12PUffS <evgenypech98@gmail.com>
2025-03-31 00:55:26 +0300

    step 6

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -9,14 +9,14 @@ console.log("Find the greatest common divisor of given numbers.");
 const calculateGcd = (a, b) => {
     a = Math.abs(a);
     b = Math.abs(b);
-    while (b) [a, b] = [b, a % b];  // Исправлено & на %
+    while (b) [a, b] = [b, a % b]; 
     return a;
 };
 
-// Основная функция игры
+
 const gcd = () => {
   
-    const ROUNDS_COUNT = 3;  // Исправлено название константы
+    const ROUNDS_COUNT = 3; 
     let correctAnswers = 0;
 
     while (correctAnswers < ROUNDS_COUNT) {
@@ -24,7 +24,7 @@ const gcd = () => {
         const num2 = Math.floor(Math.random() * 50) + 1;
         const correctAnswer = calculateGcd(num1, num2);
 
-        console.log(`Question: ${num1} ${num2}`);  // Исправлены кавычки
+        console.log(`Question: ${num1} ${num2}`); 
         const userAnswer = Number(readlineSync.question('Your answer: '));
 
         if (userAnswer === correctAnswer) {
