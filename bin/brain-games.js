2a382fe9761ed9809c918eee75382e8f08e583a1
12PUffS <evgenypech98@gmail.com>
2025-03-31 12:46:09 +0300

    update

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -2,7 +2,6 @@
 // import  sayHi  from '../cli.js';
 
 import { getName } from '../src/index.js';
-
 import sayHi from '../src/cli.js';
 sayHi()
 getName()
--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import playProgressionGame from "../src/games/progression.js";
+import playProgressionGame from '../src/games/progression.js';
 
 playProgressionGame()
   
