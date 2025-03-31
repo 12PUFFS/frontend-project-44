b4b50dde7eb141e4512555f093e6d14cbabd2e37
12PUffS <evgenypech98@gmail.com>
2025-03-31 14:12:55 +0300

    update eslint.config

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,13 @@
 // eslint.config.js
 import js from '@eslint/js';  // Правильный импорт для ESLint v9+
-import globals from 'globals';
 
 export default [
   js.configs.recommended,  // Базовые правила ESLint
   {
     rules: {
       // Ваши кастомные правила
-      "semi": ["error", "always"],
-      "quotes": ["error", "single"]
+      'semi': ['error", "always'],
+      'quotes': ['error", "single']
     }
   }
 ];
